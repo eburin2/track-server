51dec7065f1a4a99357410d81a2537356fd458e1
Elliot Burin <eburin2@gmail.com>
2019-12-21 13:36:28 -0800

    setup express setup express server

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hi There');
+});
+
+
+app.listen(3000, () => {
+  console.log("Listening on Port 3000");
+});
