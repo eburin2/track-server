82c25c564fbdfd4af3b64f5cd4fc0089129d8775
Elliot Burin <eburin2@gmail.com>
2019-12-26 14:16:04 -0800

    setup user schema using the mongoose library

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
+require('./models/User');
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const authRoutes = require('./routes/authRoutes');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(authRoutes);
 
 const mongoUri = 'mongodb+srv://admin:passwordpassword@cluster0-3urre.mongodb.net/test?retryWrites=true&w=majority';
 
@@ -17,7 +23,7 @@ mongoose.connection.on('connected', () => {
 
 mongoose.connection.on('error', (err) => {
   console.error('Error connecting to mongo', err);
-})
+});
 
 app.get('/', (req, res) => {
   res.send('Hi There');
